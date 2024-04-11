878b1e1ae5f750ff35f2c2302acc7bb6a98a5def
Joyce Cui <joyce.cui20@gmail.com>
2024-04-11 20:11:14 -0300

    feat: page sentimento

--- src/app/permita-se-sentir/page.js
+++ src/app/permita-se-sentir/page.js
@@ -3,42 +3,92 @@ import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 // import { Abel } from 'next/font/google'
 export default function Sentir() {
-    let dados = [
-        {
-            imagem: "muscIcon.png",
-            nome: "RELAXAMENTO MUSCULAR",
-            texto: "Realize relaxamentos para poder reduzir a tensão muscular no corpo causada pelo estresse e sinta-se mais leve."
-        },
-        {
-            imagem: "mindIcon.png",
-            nome: "RELAXAMENTO MENTAL",
-            texto: "Nosso cérebro está em constante atuação, principalmente ao estudar e superar desafios em jogos. Conheça alguns exercícios para tranquilizar e acalmar a mente."
-        }
-    ]
     return (
         <>
             <p><Link href="/">Voltar</Link></p>
             <h2 className="nomePag pag3">Permita-se Sentir</h2>
+
+            <h3>Introdução</h3>
+            <p>
+                É uma plataforma que auxilia jovens gamers a buscar auxilio para suas questões de saúde mental sem tratar como um tabu. A partir de recursos para garantir um planejamento entre deveres e lazer, além de técnicas de relaxamento e serviço de apoio psicológico, o indivíduo apresenta um cotidiano mais leve e acolhedor.
+            </p>
             <div>
                 <section className="cardGroup">
-                    {dados.map(dado => <Card {...dado} />)}
+                    <Card></Card>
                 </section>
             </div>
         </>
     )
 }
 
-function Card({ imagem, nome, texto }) {
+function Card() {
     return (
         <article className="alinhaVert card">
-            <div className="alinhaHoriz stretch">
-            <img className="imagem" src={imagem} />
-            <div className="alinhaVert">
-                <h3 className="titulo nimbus atividade">{nome}</h3>
-                <p className="nimbus">{texto}</p>
+            <div>
+                <div>
+                    <h3 className="nimbus-sentimento">Qual seu sentimento de hoje?</h3>
+                    <div className="emojis">
+                        <input type="radio" id="excited" name="emoji" class="emoji-input" />
+                        <label for="excited" class="emoji-label"> <img src="excited.png" alt="Excited" /></label>
+
+                        <input type="radio" id="happy" name="emoji" class="emoji-input" />
+                        <label for="happy" class="emoji-label"> <img src="happy.png" alt="Happy" /></label>
+
+                        <input type="radio" id="neutral" name="emoji" class="emoji-input" />
+                        <label for="neutral" class="emoji-label"> <img src="neutral.png" alt="Neutral" /></label>
+
+                        <input type="radio" id="sad" name="emoji" class="emoji-input" />
+                        <label for="sad" class="emoji-label"> <img src="sad.png" alt="Sad" /></label>
+
+                        <input type="radio" id="depression" name="emoji" class="emoji-input" />
+                        <label for="depression" class="emoji-label"> <img src="depression.png" alt="Depression" /></label>
+                    </div>
+
+                </div>
+
+                <div className="card-content">
+                    <h3 className="nimbus-sentimento">Que plataforma você gostaria de jogar? </h3>
+
+                    <div className="checkboxes">
+                        <div className="checkbox">
+                            <input type="checkbox" class="checkbox-round"></input>
+                            <label>PlayStation</label>
+                        </div>
+
+                        <div className="checkbox">
+                            <input type="checkbox" class="checkbox-round"></input>
+                            <label>PC</label>
+                        </div>
+
+                        <div className="checkbox">
+                            <input type="checkbox" class="checkbox-round"></input>
+                            <label>Switch</label>
+                        </div>
+
+                        <div className="checkbox">
+                            <input type="checkbox" class="checkbox-round"></input>
+                            <label>XBox</label>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div>
+                    <h3 className="nimbus-sentimento">Qual gênero gostaria de jogar hoje? </h3>
+                    <div className="selecao">
+                    <select name="select">
+                        <option>FPS</option>
+                        <option>RPG</option>
+                        <option>Battle Royale</option>
+                        <option selected>Selecione.. </option>
+                    </select>
+                    </div>
+                </div>
+
+                <div className="link-resultado">
+                <Link className="botao-resultado nimbus" href={`/{nome}`}>Ver resultados</Link>
+                </div>
             </div>
-            </div>
-            <Link className="botao nimbus" href={`/{nome}`}>+ Ver mais</Link>
         </article>
     );
 }
